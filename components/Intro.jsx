a857a32d9af401a29de581e7c1e204bf7366dcf3
najwanan <66931010+najwanan@users.noreply.github.com>
2023-09-21 10:57:53 -0500

    updating links in intro section

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,6 +1,8 @@
 
 // import WorkHistoryTab from "./WorkHistoryTab"
 import SocialMedia from './SocialMedia'
+// import AmazonLogo from '../src/assets/Amazon.svg'
+// import MicrosoftLogo from '../src/assets/Microsoft.svg'
 
 
 function Bio() {
@@ -15,7 +17,7 @@ function Bio() {
         <SocialMedia />
         </div>
       <div className="grid md:grid-cols-2 mt-20 m-10 ">
-        <div className="bg-port-citron z-0">
+        <div className="bg-port-citron z-0 shadow-md shadow-yellow-900">
             <p className=" text-md md:text-xl text-left break-normal font-mono font-medium p-8 leading-loose ">
               Now this is the story all about how 
               <br/>I flipped my career, turned upside down 
@@ -35,9 +37,11 @@ function Bio() {
          
         </div>
 
-            <div className="bg-port-poppy shadow-md shadow-yellow-900 z-10 md:-ml-20 md:-mt-20">
-            <p className="text-md md:text-xl text-left break-normal font-mono font-medium p-10 leading-loose ">
-              After years of supporting tech products in big tech as a program manager, I decided I wanted to be able to build products myself. I've dedicated the past half decade to that, honing the skillset necessary to become a technologist and using that skillset to partner with organizations and individuals working to create a more inclusive, sustainable, and equitable tech industry.
+            <div className="bg-port-poppy shadow-md shadow-yellow-900 z-10 p-10 md:-ml-20 md:-mt-20">
+            <p className="text-md md:text-xl text-left break-normal font-mono font-medium  leading-loose ">
+              After years of supporting tech products at organizations like Amazon and Microsoft as a program manager, I decided I wanted to be able to build products myself. <br />
+              <br/>
+              I've dedicated the past half decade to just that, honing the skillset necessary to become a technologist. I use this skillset to partner with organizations and individuals working to create a more inclusive, sustainable, and equitable tech industry.
     
           </p>
           </div>
@@ -51,18 +55,22 @@ function Bio() {
 
             <div className="bg-port-citron shadow-md  shadow-yellow-900 z-10 md:-ml-20 md:-mt-20">
             <p className="text-md md:text-xl text-left break-normal font-mono font-medium pt-10 pl-10 leading-loose ">
-              Some of the highlights are:
+              Some highlights from my journey:
             </p>
             <ul className="text-left w-full p-8 list-disc list-inside font-mono">
-              <li>
-                Joined an open source project partnered with GitHub and leveraged those resources to cofound a software enginering fellowship 
+              <li className='p-4'>
+                     Joined an open source project partnered with GitHub. Leveraged those resources to co-found a <a className="text-bold text-port-turquiose" href="#learn">software engineering fellowship</a> dedicated to providing professional development and opportunities to engineers from underrepresented groups and/or non-traditional training.
               </li>
-              <li>
-                 and part of the 100Devs community and Mentor Mesh
+              <li className='p-4'>
+                     Gain and share knowledge with tech communities like <a className="text-bold text-port-turquiose" href="https://100devs.org/" target="_blank" rel="noopener noreferrer"> 100 Devs </a>community and <a className="text-bold text-port-turquiose" href="https://circle.mentormesh.io" target="_blank" rel="noopener noreferrer"> Mentor Mesh, </a>dedicated to all forms of diversity. Volunteer time to coordinate hackathons, weekly open source <a className="text-bold text-port-turquiose" href="https://www.youtube.com/playlist?list=PLBKcU7Ik-ir9_x3oIQWoTg7OF6LURrHf7" target="_blank" rel="noopener noreferrer">teaching sessions </a> and mentorship.
               </li>
-              <li>
-                Support female identifying business leaders and founders. Brand Ambassdor for Tribaja
+              <li className='p-4'>
+                    Support both allies and female identifying business leaders with their change initiatives for the tech industry . For example serving as a brand ambassador for <a className="text-bold text-port-turquiose" href="https://www.linkedin.com/company/tribaja/about/" target="_blank" rel="noopener noreferrer"> Tribaja.</a>
               </li>
+               <li className='p-4'>
+                   Sometimes I get to build cool things with my friends and bring people along to learn. I'm currently part of the team <a className="text-bold text-port-turquiose" href="https://www.linkedin.com/feed/update/urn:li:activity:7098676443542421504/" target="_blank" rel="noopener noreferrer">building products in public</a>  for Mentor Mesh.
+              </li>
+
             </ul>
           </div>
       
